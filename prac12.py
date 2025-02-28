2f1df92d4ec95d19c809675c8ff7371fd5858573
Harry Manchanda <harmanmanchanda182@gmail.com>
2025-02-28 21:19:13 +0530

    prac 12

--- prac12.py
+++ prac12.py
@@ -70,3 +70,11 @@ class Dog(Animal):
       
 spot = Dog('Spot', 53, 27, 'Ruff', 'Derek')
 print(spot.toString())
+
+class AnimalTesting:
+	def get_type(self, animal):
+		animal.get_type()
+  
+test_animals = AnimalTesting()
+test_animals.get_type(cat)
+test_animals.get_type(spot)
