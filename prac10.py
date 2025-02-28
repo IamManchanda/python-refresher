0c9b162a663553fbae19ae8f97c78bf4d401fc8a
Harry Manchanda <harmanmanchanda182@gmail.com>
2025-02-28 19:00:07 +0530

    prac 10

--- prac10.py
+++ prac10.py
@@ -6,3 +6,14 @@ print(long_string[:-6]) # all but the last six characters
 print(long_string[:4] + " is awesome") # first four characters + " is awesome"
 
 print("%c is my %s letter and my number %d number is %.5f" % ('X', 'favorite', 1, .14))
+
+print(long_string.capitalize())
+print(long_string.find("string"))
+print(long_string.isalpha())
+print(len(long_string))
+
+print(long_string.replace("string", "long sentence"))
+print(long_string.strip())
+
+quote_list = long_string.split(" ")
+print(quote_list)
